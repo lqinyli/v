6834455415ab5f160bfea815beadc9df20ea8693
Lyin <lqinyli@139.com>
2021-04-21 12:21:18 +0800

    更新文件 vipParse.js

--- mb/vipParse.js
+++ mb/vipParse.js
@@ -1,6 +1,54 @@
 {
   "data": [
     {
+      "id": 10,
+      "name": "线路1",
+      "ad": "llrnlzl",
+      "url": "http://parse.aikanll.com/index.php?url="
+    },
+    {
+      "id": 12,
+      "name": "线路2",
+      "ad": "3lga3,0755tt,dlswzw,kdmmm,tiantiankaiche",
+      "url": "http://parse.aikanll.com/index1.php?url="
+    },
+    {
+      "id": 13,
+      "name": "线路3",
+      "ad": "tiantiankaiche,newimg.html,coding/5055",
+      "url": "http://parse.aikanll.com/index2.php?url="
+    },
+    {
+      "id": 14,
+      "name": "线路4",
+      "ad": "3y6c,0755tt",
+      "url": "http://parse.aikanll.com/index3.php?url="
+    },
+    {
+      "id": 15,
+      "name": "线路5",
+      "ad": "llrnlzl,0755tt",
+      "url": "http://parse.aikanll.com/index4.php?url="
+    },
+    {
+      "id": 16,
+      "name": "线路6",
+      "ad": "0755tt,adinisp,tjbhyzs,whsbpw",
+      "url": "http://parse.aikanll.com/index5.php?url="
+    },
+    {
+      "id": 17,
+      "name": "线路7",
+      "ad": "innvitor,mieyisi",
+      "url": "http://jx.drgxj.com/?url="
+    },
+    {
+      "id": 18,
+      "name": "线路8",
+      "ad": "fengchedaomall,coding/5055",
+      "url": "https://jiexi.bm6ig.cn/?url="
+    },
+    {
       "id": 19,
       "name": "线路8",
       "ad": "no",
