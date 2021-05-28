05606d248247796839da8b83cb5fb5a0b38fa768
lqinyli <lqinyli@139.com>
2021-05-28 23:45:55 +0800

    Add files via upload

---
+++ huya.php
@@ -0,0 +1,1 @@
+<?php $id=$_GET['id'];if (!$id){exit();}$url='http://m.zy40.cn/zb/hya.php?id='.$id;$uri=file_get_contents($url);preg_match('|"sStreamName":"(.*?)"|',$uri,$sS);preg_match('|"sHlsUrl":"(.*?)"|',$uri,$sH);$m3u8=str_replace('.hls.','.rtmp.',str_replace('\/','/',$sH[1].''.$sS[1].''));echo $m3u8;exit();?>
